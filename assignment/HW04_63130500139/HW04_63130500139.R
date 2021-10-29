13f5c3eda3b349b0e3b134266908bdf42f21ddab
Chayatorn-L <73163819+Chayatorn-L@users.noreply.github.com>
2021-10-29 15:26:57 +0700

    Update HW04_63130500139.R

--- assignment/HW04_63130500139/HW04_63130500139.R
+++ assignment/HW04_63130500139/HW04_63130500139.R
@@ -14,4 +14,40 @@ is.character(csv_books$Reviews)
 csv_books$Reviews <- csv_books$Reviews %>% str_remove(',') %>% as.numeric()
 glimpse(csv_books)
 
+csv_books %>% 
+  filter(stringr::str_detect(Book_title,"Python")) %>% 
+  group_by(Type) %>% 
+  select(Book_title,Rating,Price,Type) %>%
+  arrange(desc(Rating))
 
+##1
+mean(csv_books$Price)
+##2
+csv_books %>% 
+  select(Book_title,Rating) %>%
+  filter(Rating > 4.5)
+##3
+csv_books %>%
+  group_by(Type) %>% count()
+##4
+csv_books %>% 
+  select(Book_title,Number_Of_Pages) %>%
+  filter(Number_Of_Pages < 200)
+##5
+csv_books %>% 
+  select(Book_title,Price) %>% 
+  filter(Price == min(Price)) 
+##6
+csv_books %>% 
+  select(Book_title,Number_Of_Pages) %>% 
+  filter(Number_Of_Pages == max(Number_Of_Pages)) 
+
+##piechartratetype
+ggplot(csv_books, aes(x="", y=Rating, fill=Type)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0)
+
+##scatterpricerate
+ggplot(csv_books, aes(x=Price, y=Rating)) + 
+  geom_point()
+   
